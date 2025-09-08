03cf8a2658fe800d23f7b6ef1a0e6d4c2d629053
mdazam26 <azam456alam@gmail.com>
2025-09-08 14:25:22 +0000

    chore:mysql to sql server

--- authentication/connection.php
+++ authentication/connection.php
@@ -1,29 +1,35 @@
 <?php
 $environment = "PRODUCTION";
 
-if($environment == "PRODUCTION"){
-	$servername = "localhost:3306";
-	$username = "mmhttc";
-	$password = "G86sr3a_9";
-	$dbname = "mmhttc_";
 
-}else{
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "database_mmhttc";
-
+if ($environment === "PRODUCTION") {
+    $serverName = "103.73.188.70";
+    $database   = "mmhttc_website";
+    $username   = "mmhttc_website_user";
+    $password   = "RedCross@123"; 
+} else {
+    $serverName = "localhost"; 
+    $database   = "database_mmhttc";
+    $username   = "root"; 
+    $password   = ""; 
 }
 
-$conn = mysqli_connect($servername,$username,$password,$dbname);
 
-if($conn)
-{
-	echo "Connection OK";
-}
-else
-{
-	echo "Connection Failed".mysqli_connect_error();
-}
+$connectionOptions = [
+    "Database" => $database,
+    "UID"      => $username,
+    "PWD"      => $password,
+    "CharacterSet" => "UTF-8"
+];
 
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+
+
+if ($conn === false) {
+    echo "Connection Failed:<br>";
+    die(print_r(sqlsrv_errors(), true));
+} else {
+    echo "Connection OK";
+}
 ?>
